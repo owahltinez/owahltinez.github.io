9e8fa28d57fb7687ade90663c15295f1a406df95
omtinez <omtinez@gmail.com>
2016-10-04 09:10:42 -0700

    Update init.bat

--- windows/init.bat
+++ windows/init.bat
@@ -1,4 +1,4 @@
 @powershell -NoProfile -ExecutionPolicy unrestricted -Command "iex ((new-object net.webclient).DownloadString('https://chocolatey.org/install.ps1'))" && SET PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin
-cinst -y googlechrome 7zip.install putty.install scriptcs filezilla treesizefree notepadplusplus.install miniconda3 git.install paint.net nodejs.install
+cinst -y googlechrome 7zip.install putty.install scriptcs filezilla treesizefree notepadplusplus.install miniconda3 git.install paint.net nodejs.install visualstudio2015community
 C:\tools\Miniconda3\Scripts\conda.exe create -y -n py2 python=2.7 anaconda
 C:\tools\Miniconda3\Scripts\conda.exe create -y -n py3 python=3.4 anaconda
