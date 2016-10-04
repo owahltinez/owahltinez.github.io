e476f46e2baeb28d684c9f7e73093d17fdebca30
omtinez <omtinez@gmail.com>
2016-10-04 07:49:26 -0700

    Update init.sh

--- linux/init.sh
+++ linux/init.sh
@@ -4,4 +4,4 @@
 wget https://raw.githubusercontent.com/omtinez/initscripts/master/linux/basic.vim -O ~/.vimrc
 
 # Bash profile
-wget https://raw.githubusercontent.com/omtinez/initscripts/master/linux/profile.bash -O ~/.bashrc
+wget https://raw.githubusercontent.com/omtinez/initscripts/master/linux/profile.bash -O ~/.bashrc; source ~/.bashrc
