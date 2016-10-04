2f264a9b23209f324b04212fd7ecd7b7ae17a183
omtinez <omtinez@gmail.com>
2016-10-04 10:19:44 -0700

    Update init.bat

--- windows/init.bat
+++ windows/init.bat
@@ -1,3 +1,4 @@
 @powershell -NoProfile -ExecutionPolicy unrestricted -Command "iex ((new-object net.webclient).DownloadString('https://chocolatey.org/install.ps1'))" && SET PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin
 cinst -y googlechrome 7zip.install putty.install scriptcs filezilla treesizefree notepadplusplus.install miniconda3 git.install paint.net nodejs.install visualstudio2015community
 @powershell -NoProfile -ExecutionPolicy unrestricted -Command "New-Item -ErrorAction Ignore -ItemType directory -Path ~/Bin; $LOCALBIN=(Resolve-Path ~/Bin).Path; SETX /M PATH \"$LOCALBIN;$PATH\""
+@powershell -NoProfile -ExecutionPolicy unrestricted -Command "Invoke-WebRequest https://raw.githubusercontent.com/omtinez/initscripts/master/windows/profile.ps1 -OutFile ~/Documents/profile.ps1"
