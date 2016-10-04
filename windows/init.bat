1ecd487f176d2e9cd90918b8eec48b523f4699ab
omtinez <omtinez@gmail.com>
2016-10-04 09:53:31 -0700

    Update init.bat

--- windows/init.bat
+++ windows/init.bat
@@ -1,4 +1,3 @@
 @powershell -NoProfile -ExecutionPolicy unrestricted -Command "iex ((new-object net.webclient).DownloadString('https://chocolatey.org/install.ps1'))" && SET PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin
 cinst -y googlechrome 7zip.install putty.install scriptcs filezilla treesizefree notepadplusplus.install miniconda3 git.install paint.net nodejs.install visualstudio2015community
-C:\tools\Miniconda3\Scripts\conda.exe create -y -n py2 python=2.7 anaconda
-C:\tools\Miniconda3\Scripts\conda.exe create -y -n py3 python=3.4 anaconda
+@powershell -NoProfile -ExecutionPolicy unrestricted -Command "New-Item -ErrorAction Ignore -ItemType directory -Path ~/Bin; $LOCALBIN=(Resolve-Path ~/Bin).Path; SETX /M PATH \"$LOCALBIN;$PATH\""
