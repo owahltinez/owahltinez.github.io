6b4259a63205ae05b59778ea33d188595829d68a
omtinez <omtinez@gmail.com>
2017-01-03 18:17:05 -0500

    Update profile.bash

--- linux/profile.bash
+++ linux/profile.bash
@@ -2,7 +2,7 @@
 alias ll='ls -halF'
 alias cd..='cd ..'
 alias apti='sudo apt-get install'
-alias ls6="ip -6 addr | grep inet6 | awk -F '[ \t]+|/' '{print $3}' | grep -v ^::1 | grep -v ^fe80"
+alias ls6='ip -6 addr | grep inet6 | awk -F '"'"'[ \t]+|/'"'"' '"'"'{print $3}'"'"' | grep -v ^::1 | grep -v ^fe80'
 
 # Handy exports
 export GH='https://github.com/omtinez'
