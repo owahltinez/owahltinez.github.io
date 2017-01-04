3c3cd6dd45c94d4a6603fcb2f28dc6e3c3704ffd
omtinez <omtinez@gmail.com>
2017-01-03 19:14:02 -0500

    Update profile.bash

--- linux/profile.bash
+++ linux/profile.bash
@@ -2,7 +2,14 @@
 alias ll='ls -halF'
 alias cd..='cd ..'
 alias apti='sudo apt-get install'
-alias ls6='ip -6 addr | grep inet6 | awk -F '"'"'[ \t]+|/'"'"' '"'"'{print $3}'"'"' | grep -v ^::1 | grep -v ^fe80'
+
+# Define functions
+lsipv6() {
+    ip -6 addr | grep inet6 | awk -F '[ \t]+|/' '{print $3}' | grep -v ^::1 | grep -v ^fe80
+}
+
+# Export defined functions
+export -f lsipv6
 
 # Handy exports
 export GH='https://github.com/omtinez'
