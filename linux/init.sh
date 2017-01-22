07970731c9ad795c299775587a2bd9c3fda039c6
omtinez <omtinez@gmail.com>
2017-01-22 11:40:43 -0800

    Update init.sh

--- linux/init.sh
+++ linux/init.sh
@@ -4,6 +4,7 @@
 wget https://raw.githubusercontent.com/omtinez/initscripts/master/linux/basic.vim -O ~/.vimrc
 
 # Bash profile (append to existing one)
+sed -i '/### CUSTOM CONFIG STARTS HERE ###/Q' ~/.bashrc
 curl -sS https://raw.githubusercontent.com/omtinez/initscripts/master/linux/profile.bash >> ~/.bashrc
 
 # Create local bin dir and move some utils there
