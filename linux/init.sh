c547676057f0ecbba273be9477817d9d7dda4060
omtinez <omtinez@gmail.com>
2017-01-22 12:07:14 -0800

    Update init.sh

--- linux/init.sh
+++ linux/init.sh
@@ -10,6 +10,5 @@ curl -sS https://raw.githubusercontent.com/omtinez/initscripts/master/linux/prof
 # Create local bin dir and move some utils there
 mkdir -p ~/bin
 wget https://raw.githubusercontent.com/omtinez/initscripts/master/linux/switchip.sh -O ~/bin/switchip
-wget https://raw.githubusercontent.com/omtinez/initscripts/master/linux/gitsetup.sh -O ~/bin/gitsetup
 wget https://raw.githubusercontent.com/omtinez/AP-Hotspot/master/ap-hotspot -O ~/bin/ap-hotspot
 chmod +x ~/bin/*
