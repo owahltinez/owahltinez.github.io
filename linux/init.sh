84a48ed75382de07f3459e9d25730501a7e659ef
omtinez <omtinez@gmail.com>
2017-01-22 12:11:23 -0800

    Update init.sh

--- linux/init.sh
+++ linux/init.sh
@@ -5,10 +5,10 @@ wget https://raw.githubusercontent.com/omtinez/initscripts/master/linux/basic.vi
 
 # Bash profile (append to existing one)
 sed -i '/### CUSTOM CONFIG STARTS HERE ###/Q' ~/.bashrc
-curl -sS https://raw.githubusercontent.com/omtinez/initscripts/master/linux/profile.bash >> ~/.bashrc
+wget -q https://raw.githubusercontent.com/omtinez/initscripts/master/linux/profile.bash -O - >> ~/.bashrc
 
 # Create local bin dir and move some utils there
 mkdir -p ~/bin
-wget https://raw.githubusercontent.com/omtinez/initscripts/master/linux/switchip.sh -O ~/bin/switchip
-wget https://raw.githubusercontent.com/omtinez/AP-Hotspot/master/ap-hotspot -O ~/bin/ap-hotspot
+wget -q https://raw.githubusercontent.com/omtinez/initscripts/master/linux/switchip.sh -O ~/bin/switchip
+wget -q https://raw.githubusercontent.com/omtinez/AP-Hotspot/master/ap-hotspot -O ~/bin/ap-hotspot
 chmod +x ~/bin/*
