89bca05b7f3117d96158bbc3a5c7c2e90684236d
omtinez <omtinez@gmail.com>
2017-01-26 07:24:01 +0000

    Update init.sh

--- linux/init.sh
+++ linux/init.sh
@@ -1,15 +1,21 @@
 #!/bin/sh
 
+# Hard code the username
+$USERNAME=omtinez
+
 # VIM customizations
-wget --quiet https://raw.githubusercontent.com/omtinez/initscripts/master/linux/basic.vim -O ~/.vimrc
+wget --quiet https://gitlab.com/$USERNAME/initscripts/raw/master/linux/basic.vim -O ~/.vimrc
 
 # Bash profile (append to existing one)
 sed -i '/### CUSTOM CONFIG STARTS HERE ###/Q' ~/.bashrc
-wget --quiet https://raw.githubusercontent.com/omtinez/initscripts/master/linux/profile.bash -O - >> ~/.bashrc
+wget --quiet https://gitlab.com/$USERNAME/initscripts/raw/master/linux/profile.bash -O - >> ~/.bashrc
+
+# Deploy SSH keys
+wget --quiet https://gitlab.com/$USERNAME.keys -O - >> ~/.ssh/authorized_keys
 
 # Create local bin dir and move some utils there
 mkdir -p ~/bin
-wget --quiet https://raw.githubusercontent.com/omtinez/initscripts/master/linux/switchip.sh -O ~/bin/switchip
+wget --quiet https://gitlab.com/$USERNAME/initscripts/raw/master/linux/switchip.sh -O ~/bin/switchip
 wget --quiet https://raw.githubusercontent.com/omtinez/AP-Hotspot/master/ap-hotspot -O ~/bin/ap-hotspot
 chmod +x ~/bin/*
 
