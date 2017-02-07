1ad4c1cc9763cf644319f350e8382b8c765b879c
omtinez <omtinez@gmail.com>
2017-02-07 21:41:39 +0000

    Update profile.bash

--- linux/profile.bash
+++ linux/profile.bash
@@ -19,6 +19,7 @@ gitsetup() {
     git config --global credential.helper 'cache --timeout=999999999'
     git config --global user.name "omtinez"
     git config --global user.email "omtinez@gmail.com"
+    git config --global push.default simple
 }
 
 # Export defined functions
