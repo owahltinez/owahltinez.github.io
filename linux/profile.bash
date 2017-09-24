07e4815b0860259e1edc91848c2929d440ad0b0a
omtinez <omtinez@gmail.com>
2017-09-24 20:47:39 +0000

    Update profile.bash

--- linux/profile.bash
+++ linux/profile.bash
@@ -23,8 +23,11 @@ sshkeypull() {
     cat /tmp/$@.pub >> ~/.ssh/authorized_keys
     rm /tmp/$@.pub
 }
-nodeinstall() {
+install_node() {
     curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash - && apti nodejs
+}
+install_chrome() {
+    sudo sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list' && apti google-chrome-stable
 }
 gitsetup() {
     git config --global credential.helper 'cache --timeout=999999999'
