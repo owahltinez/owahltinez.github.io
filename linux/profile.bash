de182f53b28023275a58a47228897eb13523f149
omtinez <omtinez@gmail.com>
2017-09-24 20:51:54 +0000

    Update profile.bash

--- linux/profile.bash
+++ linux/profile.bash
@@ -28,8 +28,8 @@ install_node() {
 }
 install_chrome() {
     wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -  && \
-        sudo sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list' && \
-        apti google-chrome-stable
+        echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" > /etc/apt/sources.list.d/google-chrome.list && \
+        update && apti google-chrome-stable
 }
 gitsetup() {
     git config --global credential.helper 'cache --timeout=999999999'
