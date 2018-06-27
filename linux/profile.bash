d28b866f0269432271d2afdf82fcb27e1c9eeb3c
omtinez <omtinez@gmail.com>
2018-06-27 04:00:30 +0000

    Update profile.bash

--- linux/profile.bash
+++ linux/profile.bash
@@ -3,7 +3,7 @@
 # Aliases
 alias ll='ls -halF'
 alias cd..='cd ..'
-alias apti='sudo -E apt-get -yq --no-install-suggests --no-install-recommends install'
+alias apti='sudo apt-get -yq --no-install-suggests --no-install-recommends install'
 alias sudosu='sudo bash --init-file ~/.bashrc'
 alias sudovi='sudo vi -u ~/.vimrc'
 if [[ $(id -u) = 0 ]] ; then
