aa0a35421f8cd01cc35a843d4c6a6779813e8b3f
omtinez <omtinez@gmail.com>
2018-08-10 22:13:14 +0000

    Use curl or wget

--- linux/init.sh
+++ linux/init.sh
@@ -3,26 +3,31 @@
 # Hard code the username
 USERNAME="omtinez"
 
+# Use wget or curl, whichever is available (but prefer curl)
+command -v wget && DL="wget --quiet -O -"
+command -v curl && DL="curl -sSL"
+command -v $DL || { echo >&2 "wget or curl not available"; exit 1; }
+
 # VIM customizations
-wget --quiet https://gitlab.com/$USERNAME/initscripts/raw/master/linux/basic.vim -O ~/.vimrc
+$DL https://gitlab.com/$USERNAME/initscripts/raw/master/linux/basic.vim > ~/.vimrc
 
 # Bash profile (append to existing one)
 sed -i '/### CUSTOM CONFIG STARTS HERE ###/Q' ~/.bashrc
-wget --quiet https://gitlab.com/$USERNAME/initscripts/raw/master/linux/profile.bash -O - >> ~/.bashrc
+$DL https://gitlab.com/$USERNAME/initscripts/raw/master/linux/profile.bash >> ~/.bashrc
 
 # Global .gitignore
-wget --quiet https://gitlab.com/$USERNAME/initscripts/raw/master/git/.gitignore_global -O - > ~/.gitignore_global
+$DL https://gitlab.com/$USERNAME/initscripts/raw/master/git/.gitignore_global > ~/.gitignore_global
 
 # Deploy SSH keys
 mkdir -p ~/.ssh
-wget --quiet https://gitlab.com/$USERNAME.keys -O - >> ~/.ssh/authorized_keys
+$DL https://gitlab.com/$USERNAME.keys >> ~/.ssh/authorized_keys
 echo "" >> ~/.ssh/authorized_keys
 
 # Create local bin dir and move some utils there
 mkdir -p ~/bin
-wget --quiet https://gitlab.com/$USERNAME/initscripts/raw/master/linux/switchip.sh -O ~/bin/switchip
-wget --quiet https://raw.githubusercontent.com/omtinez/AP-Hotspot/master/ap-hotspot -O ~/bin/ap-hotspot
-wget --quiet https://gitlab.com/$USERNAME/initscripts/raw/master/linux/letsencrypt.sh -O ~/bin/letsencrypt.sh
+$DL https://gitlab.com/$USERNAME/initscripts/raw/master/linux/switchip.sh > ~/bin/switchip
+$DL https://raw.githubusercontent.com/omtinez/AP-Hotspot/master/ap-hotspot > ~/bin/ap-hotspot
+$DL https://gitlab.com/$USERNAME/initscripts/raw/master/linux/letsencrypt.sh > ~/bin/letsencrypt.sh
 chmod +x ~/bin/*
 
 # Output success
