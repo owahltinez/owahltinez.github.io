e55f11d8ef2e1ea5f7c59f0f6b015d31a7f7fb71
omtinez <omtinez@gmail.com>
2018-09-16 04:35:47 +0000

    ssh_screen

--- linux/profile.bash
+++ linux/profile.bash
@@ -16,7 +16,7 @@ fi
 if command -v curl > /dev/null 2>&1; then alias dl='curl -sSL' ; else alias dl='wget -O -' ; fi
 
 # Define functions
-update() {
+function update() {
     DEBIAN_FRONTEND=noninteractive \
         sudo apt-get update && \
         sudo apt-get -yq --no-install-suggests --no-install-recommends \
@@ -27,24 +27,24 @@ update() {
 }
 export -f update
 
-ssh_key_gen() {
+function ssh_key_gen() {
     mkdir -p ~/.ssh && ssh-keygen -f ~/.ssh/id_rsa -t rsa -N ''
 }
 export -f ssh_key_gen
 
-ssh_key_push() {
+function ssh_key_push() {
     ssh-copy-id -i ~/.ssh/id_rsa $@
 }
 export -f ssh_key_push
 
-ssh_key_pull() {
+function ssh_key_pull() {
     scp $@:~/.ssh/id_rsa.pub /tmp/$@.pub
     cat /tmp/$@.pub >> ~/.ssh/authorized_keys
     rm /tmp/$@.pub
 }
 export -f ssh_key_pull
 
-ssh_pwd_disable() {
+function ssh_pwd_disable() {
     # https://gist.github.com/parente/0227cfbbd8de1ce8ad05
     sudo sh -c '\
         grep -q "ChallengeResponseAuthentication" /etc/ssh/sshd_config && \
@@ -55,13 +55,13 @@ ssh_pwd_disable() {
 }
 export -f ssh_pwd_disable
 
-ssh_add_all() {
+function ssh_add_all() {
     mkdir -p ~/.ssh
     grep -slR "PRIVATE" ~/.ssh/ | xargs ssh-add
 }
 export -f ssh_add_all
 
-ssh_start_agent() {
+function ssh_start_agent() {
     mkdir -p ~/.ssh
     ssh-add -l &>/dev/null
     if [ "$?" == 2 ]; then
@@ -78,24 +78,29 @@ ssh_start_agent() {
 }
 export -f ssh_start_agent
 
-install_node() {
+function ssh_screen() {
+    ssh -i ~/keys/ssh/personal $@ -t screen -DR
+}
+export -f ssh_screen
+
+function install_node() {
     sudo curl -sL https://deb.nodesource.com/setup_8.x | bash - && apti nodejs
 }
 export -f install_node
 
-install_chrome() {
+function install_chrome() {
     dl https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -  && \
         echo "deb [arch=$(dpkg --print-architecture)] http://dl.google.com/linux/chrome/deb/ stable main" | sudo tee /etc/apt/sources.list.d/google-chrome.list && \
         update && apti google-chrome-stable
 }
 export -f install_chrome
 
-install_nginx() {
+function install_nginx() {
     sudo apti software-properties-common && add-apt-repository ppa:nginx/stable && update && apti nginx
 }
 export -f install_nginx
 
-install_acme() {
+function install_acme() {
     dl https://raw.githubusercontent.com/Neilpang/acme.sh/master/acme.sh | sudo tee /usr/local/bin/acme > /dev/null && sudo chmod +x /usr/local/bin/acme
 }
 export -f install_acme
@@ -108,7 +113,7 @@ install_docker() {
 }
 export -f install_docker
 
-install_dotnet() {
+function install_dotnet() {
     dl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg && \
     sudo mv microsoft.gpg /etc/apt/trusted.gpg.d/microsoft.gpg && \
     echo "deb [arch=$(dpkg --print-architecture)] https://packages.microsoft.com/repos/microsoft-ubuntu-bionic-prod bionic main" | sudo tee -a /etc/apt/sources.list.d/microsoft.list && \
@@ -116,7 +121,7 @@ install_dotnet() {
 }
 export -f install_dotnet
 
-install_vscode() {
+function install_vscode() {
     dl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg && \
     sudo mv microsoft.gpg /etc/apt/trusted.gpg.d/microsoft.gpg && \
     echo "deb [arch=$(dpkg --print-architecture)] https://packages.microsoft.com/repos/vscode stable main" | sudo tee -a /etc/apt/sources.list.d/microsoft.list && \
@@ -124,7 +129,7 @@ install_vscode() {
 }
 export -f install_vscode
 
-git_setup() {
+function git_setup() {
     git config --global credential.helper 'cache --timeout=999999999'
     git config --global user.name "omtinez"
     git config --global user.email "omtinez@gmail.com"
@@ -133,7 +138,7 @@ git_setup() {
 }
 export -f git_setup
 
-git_new_project() {
+function git_new_project() {
     if [[ ! $GITLAB_TOKEN ]] ; then echo "Env variable GITLAB_TOKEN has not been set" && return 1; fi
     CURR_DIR=${PWD##*/}
     PROJECT_NAME=${1:-$CURR_DIR}
