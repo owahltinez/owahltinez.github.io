148c94b037fe2a26e84f47c9de922fe4f6608dee
omtinez <omtinez@gmail.com>
2019-11-02 11:35:58 +0000

    Update init.sh

--- linux/init.sh
+++ linux/init.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-# Hard code the username
+# Hardcode the Gitlab username
 USERNAME="omtinez"
 
 # Use wget or curl, whichever is available (but prefer curl)
@@ -10,9 +10,21 @@ command -v $DL > /dev/null 2>&1 || { echo >&2 "wget or curl not available"; exit
 # VIM customizations
 $DL https://gitlab.com/$USERNAME/initscripts/raw/master/linux/basic.vim > ~/.vimrc
 
-# Bash profile (append to existing one)
-sed -i '/### CUSTOM CONFIG STARTS HERE ###/Q' ~/.bashrc
-$DL https://gitlab.com/$USERNAME/initscripts/raw/master/linux/profile.bash >> ~/.bashrc
+# Download bash profile into separate file
+$DL https://gitlab.com/$USERNAME/initscripts/raw/master/linux/profile.bash >> ~/.profile.bash
+
+# Bash RC is different in OSX
+BASH_RC="$HOME/.bashrc"
+if [[ "$OSTYPE" == "darwin"* ]]; then
+    BASH_RC=".bash_profile"
+fi
+
+# Bash profile (add ref to existing one)
+PROFILE_REF='source ~/.profile.bash'
+if ! grep -q $PROFILE_REF ~/.bashrc; then 
+    cp $HOME/.bashrc $HOME/.bashrc-$(date --iso-8601)-$(date +%s)
+    echo $PROFILE_REF >> ~/.bashrc
+fi
 
 # Global .gitignore
 $DL https://gitlab.com/$USERNAME/initscripts/raw/master/git/.gitignore_global > ~/.gitignore_global
