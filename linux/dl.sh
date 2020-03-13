5846a9fc2e99e3082df21ca73ea6b09feab86cc9
omtinez <omtinez@gmail.com>
2020-03-13 04:50:26 +0000

    Update dl.sh

--- linux/dl.sh
+++ linux/dl.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-if command -v curl > /dev/null 2>&1
-    then curl -sSL $@
-else 
-    alias wget -O - $@
+if command -v curl > /dev/null 2>&1 ; then
+    curl -sSL $@
+else
+    wget -O - $@
 fi
