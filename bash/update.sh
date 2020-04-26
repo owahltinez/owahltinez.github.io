9b903b8409bad5bb36806195dab0ceb84043d0be
Oscar Wahltinez <41451293+owahltinez@users.noreply.github.com>
2020-04-26 17:11:06 -0400

    Update update.sh

--- bash/update.sh
+++ bash/update.sh
@@ -2,11 +2,11 @@
 set -xe
 
 export DEBIAN_FRONTEND=noninteractive
-fakesudo apt-get update \
-    -o Dpkg::Progress-Fancy="1"
-fakesudo apt-get -yq --no-install-suggests --no-install-recommends \
-    -o Dpkg::Progress-Fancy="1" \
+APTGET="fakesudo apt-get -o Dpkg::Progress-Fancy="1""
+$APTGET update
+$APTGET \
     -o Dpkg::Options::="--force-confdef" \
-    -o Dpkg::Options::="--force-confold" dist-upgrade
-fakesudo apt-get -yq autoremove
+    -o Dpkg::Options::="--force-confold" \
+    -yq --no-install-suggests --no-install-recommends dist-upgrade
+$APTGET -yq autoremove
 dl https://owahltinez.github.io/linux/init.sh | sh
