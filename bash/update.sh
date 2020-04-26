95d82c9f8c6d5f42dc561c70ed7f57cf8c27a3af
Oscar Wahltinez <41451293+owahltinez@users.noreply.github.com>
2020-04-26 17:05:07 -0400

    Add fancy progress bar

--- bash/update.sh
+++ bash/update.sh
@@ -2,9 +2,11 @@
 set -xe
 
 export DEBIAN_FRONTEND=noninteractive
-fakesudo apt-get update
+fakesudo apt-get update \
+    -o Dpkg::Progress-Fancy="1"
 fakesudo apt-get -yq --no-install-suggests --no-install-recommends \
-        -o Dpkg::Options::="--force-confdef" \
-        -o Dpkg::Options::="--force-confold" dist-upgrade
+    -o Dpkg::Progress-Fancy="1"
+    -o Dpkg::Options::="--force-confdef" \
+    -o Dpkg::Options::="--force-confold" dist-upgrade
 fakesudo apt-get -yq autoremove
 dl https://owahltinez.github.io/linux/init.sh | sh
