3f3a7ee0b34f41ab23190f0d3440c6ea393a759f
Oscar Wahltinez <41451293+owahltinez@users.noreply.github.com>
2020-04-26 17:06:37 -0400

    Add fancy progress bar

--- bash/apti.sh
+++ bash/apti.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 set -xe
-sudo apt-get -yq --no-install-suggests --no-install-recommends install $@
+sudo apt-get \
+    -o Dpkg::Progress-Fancy="1" \
+    -yq --no-install-suggests --no-install-recommends install $@
